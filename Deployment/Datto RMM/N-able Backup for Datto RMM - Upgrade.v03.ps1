107bff544777c9d2aadefcebf5cc76f8e1faa244
Backup Nerd <69197737+BackupNerd@users.noreply.github.com>
2021-12-09 06:16:11 -0500

    Updates

---
+++ Deployment/Datto RMM/N-able Backup for Datto RMM - Upgrade.v03.ps1
@@ -0,0 +1,44 @@
+<## ----- About: ----
+    # DATTO RMM - Upgrade N-able Backup Manager
+    # Revision v03 - 2021-12-09
+    # Author: Eric Harless, Head Backup Nerd - N-able 
+    # Twitter @Backup_Nerd  Email:eric.harless@n-able.com
+    # Reddit https://www.reddit.com/r/Nable/
+
+<# ----- Legal: ----
+    # Sample scripts are not supported under any N-able support program or service.
+    # The sample scripts are provided AS IS without warranty of any kind.
+    # N-able expressly disclaims all implied warranties including, warranties
+    # of merchantability or of fitness for a particular purpose. 
+    # In no event shall N-able or any other party be liable for damages arising
+    # out of the use of or inability to use the sample scripts.
+# -----------------------------------------------------------#>  ## Legal
+
+<# ----- Behavior: ----
+    # D\L and deploy a NEW Backup Manager 'Documents' device for Window Workstations
+    #          
+    # Copy this Script into DATTO RMM
+    # Execute Script as Administrator 
+    #
+    # DATTO RMM SCRIPT INPUT VARIABLES NOT REQUIRED
+    #
+    # https://documentation.n-able.com/backup/userguide/documentation/Content/backup-manager/backup-manager-installation/regular-install.htm
+    # https://documentation.n-able.com/backup/userguide/documentation/Content/backup-manager/backup-manager-installation/silent.htm
+    # https://documentation.n-able.com/backup/userguide/documentation/Content/backup-manager/backup-manager-installation/reinstallation.htm
+# -----------------------------------------------------------#>
+
+###START SCRIPT###
+
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+$Url = "https://cdn.cloudbackup.management/maxdownloads/mxb-windows-x86_x64.exe"
+$Path = "C:\windows\temp\mxb-windows-x86_x64.exe"
+$Username = ""
+$Password = ""
+ 
+$WebClient = New-Object System.Net.WebClient
+$WebClient.Credentials = New-Object System.Net.Networkcredential($Username, $Password)
+$WebClient.DownloadFile( $url, $path )
+
+Start-Process -FilePath "c:\windows\temp\mxb-windows-x86_x64.exe"
+
+###END SCRIPT###
