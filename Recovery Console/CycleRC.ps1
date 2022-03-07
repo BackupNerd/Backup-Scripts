4bad5985f2e1f759532c28f108184dbd86083e67
Backup Nerd <eric.harless@n-able.com>
2022-03-07 02:48:20 -0500

    Add files via upload

---
+++ Recovery Console/CycleRC.ps1
@@ -0,0 +1,14 @@
+﻿clear-host
+Get-Process -Name 'RecoveryConsole' -ErrorAction SilentlyContinue
+Stop-Process -Name 'RecoveryConsole' -Force -ErrorAction SilentlyContinue
+
+
+Get-Service 'Recovery Console Service' -ErrorAction SilentlyContinue
+Stop-Service 'Recovery Console Service' -Force -ErrorAction SilentlyContinue
+Stop-Process -Name BackupFP -Force -ErrorAction SilentlyContinue
+Start-Service 'Recovery Console Service' -ErrorAction SilentlyContinue
+
+Start-Process -FilePath "C:\Program Files\RecoveryConsole\RecoveryConsole.exe" -ArgumentList "service-managed" -ErrorAction SilentlyContinue
+
+#Get-Process -ComputerName savage -Name RecoveryConsole
+#Get-Service -ComputerName Savage -name 'Recovery Console Service'
