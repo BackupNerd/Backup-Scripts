ade2171759b8e5986d041f32e043e5a93f6e7468
Backup Nerd <eric.harless@n-able.com>
2024-06-10 17:22:55 -0400

    Update N-able_Backup_macOSAutoDeploySingleLine.v03.sh

--- Deployment/macOS/N-able_Backup_macOSAutoDeploySingleLine.v03.sh
+++ Deployment/macOS/N-able_Backup_macOSAutoDeploySingleLine.v03.sh
@@ -1,6 +1,6 @@
 <<-----About----
     # N-able Backup for macOS - Automatic Deployment
-    # Revision v03 - 2021-12-12
+    # Revision v04 - 2024-06-07
     # Author: Eric Harless, Head Backup Nerd - N-able 
     # Twitter @Backup_Nerd  Email:eric.harless@n-able.com
     # Reddit https://www.reddit.com/r/Nable/
@@ -16,9 +16,9 @@
 -----Legal----
 
 <<-----Compatibility----
-    # For use with the Standalone edition of N-able Backup
-    # Tested with N-able Backup 21.10
-    # Tested with N-able TakeControl System Shell and Terminal on macOS 11.6 & 10.15
+    # For use with N-able | Cove Data Protection
+    # Tested with N-able Cove Data Protetion | Backup Manager 24.3
+    # Tested with N-able TakeControl System Shell, mac OS Terminal & JAMF on macOS 12.7.4, 11.7.4, 11.6, 10.15
 -----Compatibility----
 
 <<-----Behavior----
@@ -32,29 +32,29 @@
     # Type: String Variable 
     # Value: 9696c2af4-678a-4727-9b6b-example
     # Note: Found @ Backup.Management | Customers
-    # Note: https://documentation.n-able.com/backup/troubleshooting/Content/kb/MSP-How-to-turn-on-Automatic-Deployment-and-generate-a-UID-num
+    # Note: https://documentation.n-able.com/covedataprotection/USERGUIDE/documentation/Content/service-management/console-new/customer-uid.htm
     #
     # Name: PROFILE_ID
     # Type: Integer Variable 
     # Value: ProfileID#
     # Note: Found @ Backup.Management | Profiles
     #
-    # https://documentation.n-able.com/backup/userguide/documentation/Content/backup-manager/backup-manager-installation/regular-install.htm
-    # https://documentation.n-able.com/backup/userguide/documentation/Content/backup-manager/backup-manager-installation/silent-mac.htm
-    # https://documentation.n-able.com/backup/userguide/documentation/Content/backup-manager/backup-manager-installation/reinstallation.htm
-    # https://documentation.n-able.com/backup/userguide/documentation/Content/backup-manager/backup-manager-installation/full-disk-access.htm
-    # https://documentation.n-able.com/backup/userguide/documentation/Content/backup-manager/backup-manager-installation/uninstall-mac.htm
+    # https://documentation.n-able.com/covedataprotection/USERGUIDE/documentation/Content/backup-manager/backup-manager-installation/regular-install.htm
+    # https://documentation.n-able.com/covedataprotection/USERGUIDE/documentation/Content/backup-manager/backup-manager-installation/silent-mac.htm
+    # https://documentation.n-able.com/covedataprotection/USERGUIDE/documentation/Content/backup-manager/backup-manager-installation/reinstallation.htm
+    # https://documentation.n-able.com/covedataprotection/USERGUIDE/documentation/Content/backup-manager/backup-manager-installation/full-disk-access.htm
+    # https://documentation.n-able.com/covedataprotection/USERGUIDE/documentation/Content/backup-manager/backup-manager-installation/uninstall-mac.htm
 
 -----Behavior----
 
-# Begin Install Script (root authentication, i.e. using N-able TakeControl System Shell, etc.)
+# Begin Install Script (root authentication, i.e. using N-able TakeControl System Shell, JAMF, etc.)
 
     BACKUP_UID="52a304d8-a2eb-489a-9168-8845e0a846dc0"; PROFILE_ID='1285550'; INSTALL="bm#$BACKUP_UID#$PROFILE_ID#.pkg"; cd /tmp; curl -o ./$INSTALL https://cdn.cloudbackup.management/maxdownloads/mxb-macosx-x86_64.pkg; installer -pkg ./$INSTALL -target /Applications; rm -f ./$INSTALL; cd /
 
 # Begin Install Script (administrative user with sudo prompt for password, i.e. macOS terminal, SSH, Putty, etc.)
 
-	BACKUP_UID="52a304d8-a2eb-489a-9168-8845e0a846dc0"; PROFILE_ID='1285550'; INSTALL="bm#$BACKUP_UID#$PROFILE_ID#.pkg"; cd ~/Downloads; curl -o ./$INSTALL https://cdn.cloudbackup.management/maxdownloads/mxb-macosx-x86_64.pkg && sudo -s installer -pkg ./$INSTALL -target /Applications; rm ./$INSTALL; cd /
+    BACKUP_UID="52a304d8-a2eb-489a-9168-8845e0a846dc0"; PROFILE_ID='1285550'; INSTALL="bm#$BACKUP_UID#$PROFILE_ID#.pkg"; cd ~/Downloads; curl -o ./$INSTALL https://cdn.cloudbackup.management/maxdownloads/mxb-macosx-x86_64.pkg && sudo -s installer -pkg ./$INSTALL -target /Applications; rm ./$INSTALL; cd /
 
-# Begin Install Script (administrative user w/ piped password to sudo, i.e. macOS terminal, SSH, Putty, etc.) **LESS SECURE - Enter sudo password in SUDOPW variable ** 
+# Begin Install Script (administrative user w/ piped password to sudo, i.e. macOS terminal, SSH, Putty, etc.) **WARNING: LESS SECURE - Entering a sudo password in the SUDOPW variable as clear text can expose passwords** 
 
     BACKUP_UID="52a304d8-a2eb-489a-9168-8845e0a846dc0"; PROFILE_ID='1285550'; SUDOPW='PASSWORD'; INSTALL="bm#$BACKUP_UID#$PROFILE_ID#.pkg"; cd ~/Downloads; curl -o ./$INSTALL https://cdn.cloudbackup.management/maxdownloads/mxb-macosx-x86_64.pkg && echo $SUDOPW | sudo -S installer -pkg ./$INSTALL -target /Applications; rm ./$INSTALL && cd /
